068d15a46d6ec2658f3b149f828bf45b513d5eb3
SnazzyBytes <snazzybytes@gmail.com>
2023-03-20 08:32:13 -0500

    improve commit message timestamp

--- android/push.sh
+++ android/push.sh
@@ -2,7 +2,7 @@
 source repo.conf
 cd ~/storage/shared/$GH_REPO
 git add .
-git commit -m "android on $(date)"
+git commit -m "android backup: $(date '+%Y-%m-%d %H:%M:%S%z')"
 git push
 cd ~
 bash -c "read -t 3 -n 1"
